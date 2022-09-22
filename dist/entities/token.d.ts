d1901f4c5d02b0ac690805e89a83d8e9c817101d
Orest <830847+otrsk@users.noreply.github.com>
2022-09-22 19:25:27 +0200

    update chainIds as per https://github.com/scroll-tech/testnet/pull/163/files#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5

--- dist/entities/token.d.ts
+++ dist/entities/token.d.ts
@@ -30,5 +30,5 @@ export declare const WETH: {
   4: Token
   5: Token
   42: Token
-  534354: Token // 534354 for STAGING, 534353 for MAIN
+  5343541: Token // 5343541 for STAGING, 534354 for MAIN
 }
