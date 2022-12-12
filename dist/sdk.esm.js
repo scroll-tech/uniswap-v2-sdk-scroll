cdf919034db8a59d8435ae15d1b7e5446d117e1f
zhengqi zhang <770166635@qq.com>
2022-12-12 09:48:25 +0800

    update FACTORY_ADDRESS

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -36,7 +36,7 @@ var Rounding
   Rounding[(Rounding['ROUND_UP'] = 2)] = 'ROUND_UP'
 })(Rounding || (Rounding = {}))
 
-var FACTORY_ADDRESS = '0x066A51a6Bc283f4D28eBd4DdE06F5B874009EdfD'
+var FACTORY_ADDRESS = '0x57426eA4e2878527F36d26d950b0d1de8c2AF2eb'
 var INIT_CODE_HASH = '0xeaa641206730108a5d03240c05597d08a25dff704ff8d9ed22f55c0229a29695'
 var MINIMUM_LIQUIDITY = /*#__PURE__*/ JSBI.BigInt(1000) // exports for internal consumption
 
